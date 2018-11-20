4d7860fe4dc94158baeb515d9195d870bf7975a3
Kassio Silva <kassio.slva@gmail.com>
2018-11-20 20:07:05 -0300

    Adicionar repositorios

---
+++ src/main.js
@@ -0,0 +1,28 @@
+class App {
+  constructor() {
+    this.repositories = [];
+
+    this.formEl = document.getElementById('repo-form');
+
+    this.registerHandlers();
+  }
+
+  registerHandlers() {
+    this.formEl.onsubmit = event => this.addRepository(event);
+  }
+
+  addRepository(event) {
+    event.preventDefault();
+
+    this.repositories.push({
+      name: 'rockeseat.com.br',
+      description: 'Tire a sua ideia do papel e dê vida a sua startup.',
+      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+      html_url: 'http://github.com/rocketseat/rocketseat.com.br',
+    });
+
+    console.log(this.repositories);
+  }
+}
+
+new App();
