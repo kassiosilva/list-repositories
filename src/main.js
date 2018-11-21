ea144958c8d4c4c4b51b0ff271d1fc7e49d9ec16
Kassio Silva <kassio.slva@gmail.com>
2018-11-20 21:30:55 -0300

    Adicionando loading e error

--- src/main.js
+++ src/main.js
@@ -15,29 +15,48 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
+  setLoading(loading = true) {
+    if (loading === true) {
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando'));
+      loadingEl.setAttribute('id', 'loading');
+
+      this.formEl.appendChild(loadingEl);
+    } else {
+      document.getElementById('loading').remove();
+    }
+  }
+
   async addRepository(event) {
     event.preventDefault();
 
     const repoInput = this.inputEl.value;
 
-    // Caso não haja nada - o return para a execução do método
     if (repoInput === 0)
       return;
 
-    const response = await api.get(`/repos/${repoInput}`);
+    this.setLoading();
 
-    const {name, description, html_url, owner: { avatar_url }} = response.data;
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
 
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url
-    });
+      const {name, description, html_url, owner: { avatar_url }} = response.data;
 
-    this.inputEl.value = '';
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url
+      });
 
-    this.render();
+      this.inputEl.value = '';
+
+      this.render();
+    } catch (err) {
+      alert('O repositório não existe!');
+    }
+
+    this.setLoading(false);
   }
 
   render() {
